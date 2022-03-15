39bfd6daaec2fefbc2491b868148684924bd46fe
Kuunapor Doreen Mwinkuma <kuunapordoreen@gmail.com>
2022-03-15 04:05:02 +0000

    Add files via upload

---
+++ jav.js
@@ -0,0 +1,4 @@
+let firstName = prompt("Hi 👋 What's your name?");
+let hi = ("Hi")
+let welcome = ("welcome to my first ever website coded by me.")
+alert(hi + " " +  firstName + ", " + " " + welcome)
